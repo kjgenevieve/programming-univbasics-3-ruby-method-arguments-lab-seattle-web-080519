eaa3ea6a198b16065c5b6f7baed97d6b0728f55a
Genevieve <kj.genevieve@gmail.com>
2019-07-16 01:27:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,2 +1,3 @@
 #def introduction that takes the argument "name" and outputs the prhase: "Hi, my name is #{name}."
 
+#Define a method, #introduction_with_language that takes in two arguments, name and language and outputs the phrase: "Hi, my name is #{name} and I am learning to program in #{language}."
