ae9ce342585c88e66a63198e4c8de2a53aec068b
Genevieve <kj.genevieve@gmail.com>
2019-07-16 01:29:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,11 +2,12 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-
-
-
-#Define a method, #introduction_with_language that takes in two arguments, name and language and outputs the phrase: "Hi, my name is #{name} and I am learning to program in #{language}."
-
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 # Copy the previous method and name it #introduction_with_language_optional. It that takes in two arguments,name and language, but language is optional. The default value for language will be "Ruby".
